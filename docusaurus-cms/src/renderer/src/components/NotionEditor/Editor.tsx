e623eb7d72aceec76922db1f0e0acf8706116f55
EphraimHaber <ephraimxyz@gmail.com>
2025-05-02 22:28:10 +0300

    feat: Add Prism.js integration for enhanced syntax highlighting in code blocks

--- docusaurus-cms/src/renderer/src/components/NotionEditor/Editor.tsx
+++ docusaurus-cms/src/renderer/src/components/NotionEditor/Editor.tsx
@@ -13,6 +13,23 @@ import SlashCommands from './SlashCommands';
 import { AdmonitionExtension } from './extensions/AdmonitionExtension';
 import { CodeBlockExtension } from './extensions/CodeBlockExtension';
 
+import css from 'highlight.js/lib/languages/css';
+import js from 'highlight.js/lib/languages/javascript';
+import ts from 'highlight.js/lib/languages/typescript';
+import html from 'highlight.js/lib/languages/xml';
+import CodeBlockLowlight from '@tiptap/extension-code-block-lowlight';
+// load all languages with "all" or common languages with "common"
+import { all, createLowlight } from 'lowlight';
+import { CodeBlockPrism } from './extensions/PrismCodeBlockExtension';
+
+// create a lowlight instance with all languages loaded
+const lowlight = createLowlight(all);
+
+lowlight.register('html', html);
+lowlight.register('css', css);
+lowlight.register('js', js);
+lowlight.register('ts', ts);
+
 interface NotionEditorProps {
   content: string;
   onChange: (markdown: string) => void;
@@ -45,7 +62,11 @@ const NotionEditor = ({ content, onChange, onSave }: NotionEditorProps): React.J
 
   const editor = useEditor({
     extensions: [
+      // CodeBlockLowlight.configure({
+      //   lowlight,
+      // }),
       // Use StarterKit but exclude the default codeBlock extension
+      CodeBlockPrism.configure({ defaultLanguage: 'jsx' }),
       StarterKit.configure({
         codeBlock: false,
       }),
@@ -61,7 +82,7 @@ const NotionEditor = ({ content, onChange, onSave }: NotionEditorProps): React.J
       Highlight,
       AdmonitionExtension,
       // Add our custom codeBlock extension
-      CodeBlockExtension,
+      // CodeBlockExtension,
       GlobalDragHandle,
     ],
     content: parsedContent,
